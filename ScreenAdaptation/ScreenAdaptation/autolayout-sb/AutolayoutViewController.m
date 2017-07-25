66ddc5d79761874d88ad82efcc62cb1823d45d1d
Rakuyo <rakuyo.mo@gmail.com>
2017-07-25 16:12:50 +0800

    1.修改了在storyboard中使用autolayout的约束，现在任务4完成了。 2.修改README文件中项目目标进度以及错误修改进度。添加新的错误修改目标。

--- ScreenAdaptation/ScreenAdaptation/autolayout-sb/AutolayoutViewController.m
+++ ScreenAdaptation/ScreenAdaptation/autolayout-sb/AutolayoutViewController.m
@@ -20,36 +20,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // 保证首次进入时，mainView的形状。
-    [self changeMainViewConstr];
-    
-    NSLog(@"状态栏高：%f----naviBar高：%f",+ [UIApplication sharedApplication].statusBarFrame.size.height, self.navigationController.navigationBar.frame.size.height);
-    
-    NSLog(@"TopConstant:%f----BottomConstant:%f",self.mainViewTop.constant, self.mainViewBottom.constant);
-    
-    NSLog(@"MainViewH:%f----MainViewW:%f",self.mainView.frame.size.height, self.mainView.frame.size.width);
-    
-    NSLog(@"ViewH:%f----ViewW:%f",self.view.frame.size.height, self.view.frame.size.width);
-    
-    NSLog(@"和：%f-------space", self.mainViewTop.constant + self.mainViewBottom.constant + self.mainView.frame.size.width);
 }
 
-- (void)changeMainViewConstr {
-    CGFloat space = (self.view.frame.size.height -  self.mainView.frame.size.width) / 2;
-    CGFloat naviH = self.navigationController.navigationBar.frame.size.height ;
-    self.mainViewTop.constant = space - naviH;
-    self.mainViewBottom.constant = space + naviH;
-    
-//    NSLog(@"状态栏高：%f----naviBar高：%f",+ [UIApplication sharedApplication].statusBarFrame.size.height, self.navigationController.navigationBar.frame.size.height);
-//    
-//    NSLog(@"TopConstant:%f----BottomConstant:%f",self.mainViewTop.constant, self.mainViewBottom.constant);
-//    
-//    NSLog(@"MainViewH:%f----MainViewW:%f",self.mainView.frame.size.height, self.mainView.frame.size.width);
-//    
-//    NSLog(@"ViewH:%f----ViewW:%f",self.view.frame.size.height, self.view.frame.size.width);
-//    
-//    NSLog(@"和：%f-------space：%f", self.mainViewTop.constant + self.mainViewBottom.constant + self.mainView.frame.size.height,space);
+- (void)viewDidLayoutSubviews {
+
+    // 如果View有Navigation的话，那么约束是从NaviBar的下面开始计算的，而不是从状态栏的下面或者屏幕顶部开始计算的。
+    CGFloat spac = ((self.view.frame.size.height - self.navigationController.navigationBar.frame.size.height - [UIApplication sharedApplication].statusBarFrame.size.height) - self.mainView.frame.size.width) * 0.5;
+    // 保证首次进入时，mainView的形状。
+    self.mainViewTop.constant = spac;
+    self.mainViewBottom.constant = spac;
 }
 
 @end
