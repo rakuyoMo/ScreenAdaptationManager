f0b7cb6297d1a27da92dcaab30a98682bd26b6c4
Rakuyo <rakuyo.mo@gmail.com>
2017-07-25 13:15:39 +0800

    移除原先用以保证旋转时使BaseView保持在屏幕正中心的监听代码，改用viewDidLayoutSubviews方法控制。

--- ScreenAdaptation/ScreenAdaptation/autoresizing-sb/AutoSBViewController.m
+++ ScreenAdaptation/ScreenAdaptation/autoresizing-sb/AutoSBViewController.m
@@ -15,41 +15,17 @@
 
 @implementation AutoSBViewController
 
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    // 屏幕旋转时使BaseView在屏幕中间。且保证无论合适进入VC，BaseView都在屏幕中间。
+    self.mainView.center = self.view.center;
+}
+
 #pragma mark 监听旋转。
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // 保证横屏进入时，BaseView在屏幕中间。
-    self.mainView.center = self.view.center;
-    
-    // 监听开始旋转
-    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeViewFrame) name:UIDeviceOrientationDidChangeNotification object:nil];
+
 }
-
-#pragma mark 移除监听。
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIDeviceOrientationDidChangeNotification object:nil];
-}
-
-#pragma mark 处理旋转。
-- (void)changeViewFrame {
-    
-    CGFloat naviBarHeight = self.navigationController.navigationBar.frame.size.height;
-    CGFloat viewHeight = self.view.frame.size.height;
-    CGFloat viewWeight = self.view.frame.size.width;
-    // 取绝对值。
-    CGFloat space = fabs( (viewHeight - viewWeight) / 2);
-    
-    // 根据屏幕旋转方向处理视图frame。
-    [self.view changeViewFrameOnCrossScreen:^{
-        self.mainView.frame = CGRectMake(space, naviBarHeight, viewHeight - naviBarHeight, viewHeight - naviBarHeight);
-        
-    } OrVerticalScreen:^{
-        self.mainView.frame = CGRectMake(0, space, viewWeight, viewWeight);
-    }];
-}
-
-
 
 @end
