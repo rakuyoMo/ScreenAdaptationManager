a9cad59e86e6d2f0a7729328c424d44e38556141
Rakuyo <rakuyo.mo@gmail.com>
2017-07-27 10:20:26 +0800

    移除autolayout_SB中用代码进行约束的部分，全部改为在storyboard中用约束搞定。 2.修改了在每次点击cell的时候都会添加ViewController的问题。 3.修改了README文件，“错误修改”现在叫“待修改问题”了。 4.修改了README文件中待修改问题的目标完成度。 5.修改了README文件中关于任务4的布局思路。 6.在README文件中添加新的笔记地址。

--- ScreenAdaptation/ScreenAdaptation/autolayout-sb/AutolayoutViewController.m
+++ ScreenAdaptation/ScreenAdaptation/autolayout-sb/AutolayoutViewController.m
@@ -10,45 +10,12 @@
 
 @interface AutolayoutViewController ()
 
-@property (weak, nonatomic) IBOutlet UIView *mainView;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *mainViewTop;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *mainViewBottom;
-
 @end
 
 @implementation AutolayoutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-//    [self.mainView setTranslatesAutoresizingMaskIntoConstraints:NO];
-//
-//    CGFloat spac = ((self.view.frame.size.height - self.navigationController.navigationBar.frame.size.height - [UIApplication sharedApplication].statusBarFrame.size.height) - self.mainView.frame.size.width) * 0.5;
-//    
-////    NSLog(@"%@-----%@",self.mainViewTop, self.mainViewBottom);
-//    
-//    // 添加 width 约束
-//    self.mainViewTop = [NSLayoutConstraint constraintWithItem:self.mainView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:spac];
-////    [self.mainView addConstraint:mainViewTop];
-//    
-//    self.mainViewBottom = [NSLayoutConstraint constraintWithItem:self.mainView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-spac];
-//    [self.mainView addConstraint:mainViewBottom];
-//    [purpleView addConstraint:widthConstraint];
-    
-//    NSLog(@"%@-----%@",self.mainViewTop, self.mainViewBottom);
-    
-//    NSLog(@"%f----%f",self.mainView.frame.size.height, self.mainView.frame.size.width);
-//    NSLog(@"%f",spac);
-
-}
-
-- (void)viewDidLayoutSubviews {
-
-    // 如果View有Navigation的话，那么约束是从NaviBar的下面开始计算的，而不是从状态栏的下面或者屏幕顶部开始计算的。
-    
-    // 保证首次进入时，mainView的形状。
-//    self.mainViewTop.constant = spac;
-//    self.mainViewBottom.constant = spac;
 }
 
 @end
